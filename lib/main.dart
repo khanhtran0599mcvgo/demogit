1eaae1d43f41cfdc6751c867e19b859f92574833
khanhtran0599 <khanhtran0599@gmail.com>
2021-10-30 17:14:48 +0700

    them color

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Text('hdsadjksajkd')
+      child:Text('hdsadjksajkd',style: TextStyle(color: Colors.white),)
     );
   }
 }
